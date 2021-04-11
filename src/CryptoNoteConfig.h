5e95fd7be1bb5a1ea743b951732335b839155a3f
Engin <engincetin@me.com>
2021-04-11 18:50:09 +0300

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -75,7 +75,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 } // parameters
 
 //TODO Put here the name of your currency
-const char     CRYPTONOTE_NAME[]                             = "HEART";
+const char     CRYPTONOTE_NAME[]                             = "ENGCOIN";
 const char GENESIS_COINBASE_TX_HEX[] = "013c01ff0001a6eaafe301029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101cbf30879a4647338cb1a87edef9406cfcb865ac355c164fab28d68f93520da5a";
 
 
